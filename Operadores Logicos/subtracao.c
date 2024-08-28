89a62cd8483629180be110c3a92705fd0787b7b7
Matheus Soares <111543203+matheussoares1@users.noreply.github.com>
2024-08-28 13:57:42 -0300

    Create subtracao.c

---
+++ Operadores Logicos/subtracao.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+  int num1, num2, sub;
+  
+  printf("Digite o priemiro numero: ");
+  scanf("%d", &num1);
+  printf("Digite o Segundo numero: ");
+  scanf("%d", &num2);
+  
+  sub = num1 + num2;
+  printf("O resultado da subtraca de %d - %d = %d", num1, num2, sub);
+  
+}
